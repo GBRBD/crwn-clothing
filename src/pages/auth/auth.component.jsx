66a857143a17ade3068932b2573cf4b4d716e31f
GBRBD <gaborattilabodi@gmail.com>
2019-09-21 23:21:31 +0200

    Add SignUp component to AuthPage

--- src/pages/auth/auth.component.jsx
+++ src/pages/auth/auth.component.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
-import './auth.styles.scss';
-import SignIn from '../../components/sign-in/sign-in.component';
 
-const AuthPage = () => {
-  return (
-    <div className='auth-page'>
-      <SignIn />
-    </div>
-  );
-};
+import SignIn from '../../components/sign-in/sign-in.component';
+import SignUp from '../../components/sign-up/sign-up.component';
+
+import './auth.styles.scss';
+
+const AuthPage = () => (
+  <div className='sign-in-and-sign-up'>
+    <SignIn />
+    <SignUp />
+  </div>
+);
 
 export default AuthPage;
