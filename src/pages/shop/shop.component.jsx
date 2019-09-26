b7828e4b35029f54ed58359340d1bda2746687d8
GBRBD <gaborattilabodi@gmail.com>
2019-09-26 20:34:16 +0200

    Create Collection Overview Component

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,16 +1,14 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
+import CollectionsOverview from '../../components/collections-overview/collections-overview.components';
 
-import CollectionPreview from '../../components/collection-preview/collection-preview.component.jsx';
 import { selectCollections } from '../../redux/shop/shop.selectors.js';
 
 const ShopPage = ({ collections }) => {
   return (
     <div className='shop-page'>
-      {collections.map(({ id, ...otherCollectionProps }) => (
-        <CollectionPreview key={id} {...otherCollectionProps} />
-      ))}
+      <CollectionsOverview />
     </div>
   );
 };
