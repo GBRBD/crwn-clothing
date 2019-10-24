880d3c615d0c66510d96140fea6d0e082e77437d
GBRBD <gaborattilabodi@gmail.com>
2019-10-24 20:08:33 +0200

    Update collection.component.jsx

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { connect } from 'react-redux';
 
 import './collection.styles.scss';
-import { selectCollection } from '../../redux/shop/shop.selectors';
 import CollectionItem from '../../components/collection-item/collection-item.component';
+import { selectCollection } from '../../redux/shop/shop.selectors';
 
 const CollectionPage = ({ collection }) => {
   const { title, items } = collection;
